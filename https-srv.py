3fe6fdd37c57e0377288389b269fd97e71b30c12
Darkerego <darkerego@users.noreply.github.com>
2015-10-11 02:16:45 -0400

    Update https-srv.py

--- https-srv.py
+++ https-srv.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python env
 # Quick&Dirty HTTPS Server
-
+##########################################
+# Upload this to a server with python \  #
+# and download whatever is the serving \ #
+# directory. Script then deletes itself. #
+##########################################
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 import os
-
+# generate our openssl key for the session
 def genKey():
 
     os.system("openssl req \
@@ -19,12 +23,12 @@ def genKey():
 
     os.system("cat /tmp/server.crt /tmp/server.key > /tmp/server.pem")
 
-
+# shred our key and delete ourself after keyboard interrupt
 def shredKey():
     os.system("srm /tmp/server.* || rm /tmp/server.*")
     os.remove((__file__))
 
-
+# serve over ssl forever
 def main():
     httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/tmp/server.pem', server_side=True)
@@ -33,6 +37,8 @@ def main():
 
 
 genKey()
+
+# shred and exit
 
 if __name__ == "__main__":
    try:
