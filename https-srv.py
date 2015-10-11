39b02be8a98445c7d6cd0aa903312f0f267dd1b6
Darkerego <darkerego@users.noreply.github.com>
2015-10-11 01:01:33 -0400

    Update https-srv.py
    Added a func to shred the certs after keyboard interupt.

--- https-srv.py
+++ https-srv.py
@@ -5,7 +5,9 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 import os
 
-os.system("openssl req \
+def genKey():
+
+    os.system("openssl req \
     -new \
     -newkey rsa:4096 \
     -days 365 \
@@ -15,12 +17,25 @@ os.system("openssl req \
     -keyout /tmp/server.key \
     -out /tmp/server.crt")
 
-os.system("cat server.crt server.key > /tmp/server.pem")
+    os.system("cat /tmp/server.crt /tmp/server.key > /tmp/server.pem")
 
 
+def shredKey():
+    os.system("srm /tmp/server.* || rm /tmp/server.*")
 
 
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/tmp/server.pem', server_side=True)
-print("Serving on 0.0.0.0:4443")
-httpd.serve_forever()
+def main():
+    httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+    httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/tmp/server.pem', server_side=True)
+    print("Serving on 0.0.0.0:4443")
+    httpd.serve_forever()
+
+
+genKey()
+
+if __name__ == "__main__":
+   try:
+      main()
+   except KeyboardInterrupt:
+      shredKey()
+      pass
